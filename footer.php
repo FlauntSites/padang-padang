6884addc4059452840ef58150e894106ad8dbecb
William Bay <william@williambay.com>
2017-03-25 09:27:08 -0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Padang_Padang
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+
+			<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.
+				<?php if ( ! is_admin() ) : ?>
+				<a href="/privacy-policy" class="privacy">Privacy Policy</a>
+				<?php endif; ?>
+			</p>
+  			
+			<p class="site-credit">
+				<?php 
+
+					if ( is_front_page() ){
+
+						printf( esc_html__( 'Website Proudly Provided by: %s.', 'padang-padang' ), '<a href="https://flauntsites.com.com/">Flaunt Sites</a>' ); 
+				}else{
+						printf( esc_html__( 'Website Proudly Provided by: %s.', 'padang-padang' ), '<a href="https://flauntsites.com.com/" rel="nofollow">Flaunt Sites</a>' );
+
+				}?>
+			</p>
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Padang_Padang
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function padang_padang_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'padang_padang_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'padang_padang_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function padang_padang_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
