2af61d579abb4ab96775275b5fec060c4c61aa01
William Bay <william@williambay.com>
2017-03-29 00:15:31 -0700

    Enqueue’s Swiper Slider scripts and CSS & Adds a FSC_Figure function for referencing ACF images into figure and figcaption markup.

--- functions.php
+++ functions.php
@@ -113,6 +113,11 @@ function padang_padang_scripts() {
 
 	wp_enqueue_script( 'padang-padang-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'swiper-slider-jquery', get_template_directory_uri() . '/inc/swiper/swiper.jquery.min.js', array( 'jquery' ), '', false );
+	wp_enqueue_script( 'swiper-slider-script', get_template_directory_uri() . '/inc/swiper/swiper.min.js', array( 'jquery' ), '', false );
+	wp_enqueue_style( 'swiper-slider-styles', get_template_directory_uri() . '/inc/swiper/swiper.css', array(), ''  );
+
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -148,4 +153,26 @@ require get_template_directory() . '/inc/jetpack.php';
 
 
 
+function fsc_figure( $image, $size, $imageclass, $captionclass ){
+
+		$image = get_field( $image );
+		$size = $size;
+		$thumb = $image['sizes'][ $size ];
+
+		if( !empty($image) ):  ?>
+
+			<figure class="<?php echo $imageclass; ?>">
+			
+				<img src="<?php echo $thumb; ?>" alt="<?php echo $image[ 'alt' ]; ?>" />
+
+					<figcaption class="<?php echo $captionclass; ?>">
+
+						<?php echo $image[ 'caption' ]; ?>
+
+					</figcaption>
+
+			</figure>
+
+		<?php endif; 
+}
 
