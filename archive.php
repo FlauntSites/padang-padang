ed75506ca8b00aa0e4d277e7ac1bc464175ea355
William Bay <william@flauntyoursite.com>
2018-07-21 20:20:53 -0700

    Added a Link to the Title.

--- archive.php
+++ archive.php
@@ -37,7 +37,7 @@ get_header(); ?>
 					<div class="small-12 medium-8 columns">					
 
 					<header class="entry-header">
-						<h2><?php the_title(); ?></h2>
+						<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 					</header>
 
 					<section>
